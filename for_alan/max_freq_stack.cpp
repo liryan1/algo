b6ae6a82588c44a7b2e93853b6a930792614d36a
ElectronCS <alan08820@gmail.com>
2022-09-08 21:20:14 -0400

    solved max_freq_stack :)

--- for_alan/max_freq_stack.cpp
+++ for_alan/max_freq_stack.cpp
@@ -1,7 +1,47 @@
+#include <deque>
+#include <unordered_map>
+#include <assert.h>
+
+using namespace std;
+
 class FreqStack
 {
 public:
-    void push(int val) {}
+    int max_freq = 0;
+    unordered_map<int, int> freq;
+    unordered_map<int, deque<int>> levels;
 
-    int pop() {}
+    void push(int val) {
+        freq[val]++;
+        max_freq = max(max_freq, freq[val]);
+        levels[freq[val]].push_back(val);
+    }
+
+    int pop() {
+        int ans = levels[max_freq].back();
+        levels[max_freq].pop_back();
+        freq[ans]--;
+        if (levels[max_freq].size() == 0) {
+            max_freq--;
+        }
+        return ans;
+    }
 };
+
+int main() {
+    FreqStack f;
+    f.push(3);
+    f.push(4);
+    f.push(4);
+    f.push(3);
+    f.push(5);
+    assert(f.pop() == 3);
+    assert(f.pop() == 4);
+    assert(f.pop() == 5);
+    f.push(6);
+    f.push(3);
+    assert(f.pop() == 3);
+    assert(f.pop() == 6);
+    assert(f.pop() == 4);
+    assert(f.pop() == 3);
+}
